61bade1b23ec1bcac2d8458c7d56d69f828c7d60
Fadlo05 <fadlo.hosaily@gmail.com>
2025-06-30 11:09:16 +0100

    go-reloaded

--- functions/atoan.go
+++ functions/atoan.go
@@ -3,9 +3,9 @@ package functions
 func AToAn(s []string) []string {
 	var res []string
 	for i := 0; i < len(s); i++ {
-		if i+1 < len(s) && s[i] == "a" && CheckVowel(s[i+1]) {
+		if i+1 < len(s) && (s[i] == "a" || s[i] == "'a" || s[i] == "(a") && CheckVowel(s[i+1]) {
 			res = append(res, "an")
-		} else if i+1 < len(s) && s[i] == "A" && CheckVowel(s[i+1]) {
+		} else if i+1 < len(s) && (s[i] == "A" || s[i] == "'A" || s[i] == "(A") && CheckVowel(s[i+1]) {
 			res = append(res, "An")
 		} else {
 			res = append(res, s[i])
